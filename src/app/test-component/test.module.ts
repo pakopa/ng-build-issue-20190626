127ec9aeedbdcdab5762d4f641aa5ee173491a91
Francisco Salvador <f.salvador@dpisa.es>
2019-06-26 15:55:53 +0200

    sample project

---
+++ src/app/test-component/test.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { TestComponent } from './test.component';
+
+@NgModule({
+	declarations: [
+		TestComponent
+	],
+	exports: [
+		TestComponent
+	]
+})
+export class TestModule { }
